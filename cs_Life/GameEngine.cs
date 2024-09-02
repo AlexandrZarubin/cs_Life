76ff52055cb832a0779addc38700206cbeffd93e
Alexander Zarubin <whitesashka@gmail.com>
2024-09-03 01:39:01 +0500

    Add project files.

---
+++ cs_Life/GameEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_Life
+{
+	public class GameEngine
+	{
+		private bool[,] field;
+		private readonly int rows;
+		private readonly int cols;
+		private Random random=new Random();
+		public GameEngine(int rows, int cols,int density)
+		{
+			this.rows = rows;
+			this.cols = cols;
+			field = new bool[cols, rows];
+			for (int x = 0; x < cols; x++)
+			{
+				for (int y = 0; y < rows; y++)
+				{
+					field[x, y] = random.Next((int)nudDensity.Value) == 0;
+				}
+			}
+		}
+
+	}
+}
